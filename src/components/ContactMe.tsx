c4578443c1488edd781cc45b72c8d194fd1e77e9
Carmine Plaitano <c.plaitano@gmail.com>
2025-10-29 12:16:14 +0100

    Update ContactMe.tsx

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,31 +1,15 @@
-import { userRef } from 'react';
-import { MdOutlineEmail } from 'react-icons/md';
-import { RiInstagramLine } from 'react-icons/ri';
-import { BsWhatsapp } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiInstagramLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 import type { FormEvent } from "react";
 import { useTranslation } from 'react-i18next';
 import SmallCard from './ui/SmallCard';
 import Button from './ui/Button';
-import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
-   const form = userRef<HTMLFormElement>(null);
-
    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-
-      emailjs
-         .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-            publicKey: 'YOUR_PUBLIC_KEY',
-         })
-         .then(
-            () => {
-               console.log('SUCCESS!');
-            },
-            (error) => {
-               console.log('FAILED...', error.text);
-            },
-         );
+      e.currentTarget.reset();
    };
 
    const { t } = useTranslation();
@@ -40,15 +24,15 @@ const ContactMe = () => {
          <div className="container contacts_container d-flex flex-column">
             <div className="contact-me_container_cards w-xl-50 d-flex flex-column md-flex-row flex-grow-1 gap-3 mx-auto">
                <SmallCard link="mailto:c.plaitano@gmail.com" target="_blank" title="Email" subtitle="c.plaitano@gmail.com" icon={MdOutlineEmail} />
-               <SmallCard link="https://ig.me/m/carmineplaitano.it" target="_blank" title="Instagram" subtitle="ig.me/m/carmineplaitano.it" icon={RiInstagramLine} />
+               <SmallCard link="https://ig.me/m/carmineplaitano.it" target="_blank" title="Instagram" subtitle="ig.me/m/carmineplaitano.it" icon={RiInstagramLine}/>
                <SmallCard link="https://wa.me/393272061751" target="_blank" title="WhatsApp" subtitle="+39 327 2061 751" icon={BsWhatsapp} />
             </div>
             <div className="contact-me_form-container w-xl-50 mx-auto mt-4">
                <form onSubmit={sendEmail} className="contact-me_form-container_form">
-                  <input type="text" name="name" placeholder={t("Full Name") + "..."} required className="contact-me_form-container_form_input" />
-                  <input type="email" name="email" placeholder="Email..." required className="contact-me_form-container_form_input" />
-                  <textarea name="message" placeholder={t("Message") + "..."} required className="contact-me_form-container_form_textarea"></textarea>
-                  <Button type="submit" className="btn btn-primary contact-me_form-container_form_button">{t("Send Message")}</Button>
+                  <input type="text" name="name" placeholder={ t("Full Name") + "..." } required className="contact-me_form-container_form_input"/>
+                  <input type="email" name="email" placeholder="Email..." required className="contact-me_form-container_form_input"/>
+                  <textarea name="message" placeholder={ t("Message") + "..." } required className="contact-me_form-container_form_textarea"></textarea>
+                  <Button type="submit" className="btn btn-primary contact-me_form-container_form_button">{ t("Send Message") }</Button>
                </form>
             </div>
          </div>
