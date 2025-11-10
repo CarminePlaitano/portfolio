d7096f5fbca89d74dac6eb0394871edc3d4a2155
Carmine Plaitano <c.plaitano@gmail.com>
2025-11-10 12:29:38 +0100

    fix/general: removed form from contact-me section and cards from about-me section; updated translations

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -15,15 +15,9 @@ const AboutMe = () => {
             <h2 className="text-center title">{t("About me")}</h2>
          </div>
          <div className="container about-me_container d-flex flex-column gap-5">
-            <div className="about-me_container_cards w-xl-50 d-flex flex-column md-flex-row flex-grow-1 gap-3 mx-auto">
-               <SmallCard title={t("Experience")} subtitle={t("3+ Years")} icon={FaAward} />
-               <SmallCard title={t("Clients")} subtitle={t("Agencies and Start-ups")} icon={FiUsers} />
-               <SmallCard title={t("Projects")} subtitle={t("20+ Completed")} icon={VscFolderLibrary} />
-            </div>
             <div className="about-me_container_text w-xl-50 mx-auto mt-2">
                <p className="text-justify mb-2">{t("My name is Carmine Plaitano and I am a Full-Stack Web Developer with 3+ years of professional experience building maintainable web applications, pragmatic frontend interfaces and APIs using PHP (Symfony, Yii2), modern JavaScript and TypeScript (React, Angular), testing tools (Vitest, PHPUnit) and other useful technologies (Docker, Git, MySQL, PostgreSQL).")}</p>
                <p className="text-justify mb-4">{t("I am passionate about coding and web development and I am always looking for new challenges and opportunities to grow professionally.")}</p>
-               <Button href="#contacts" className="btn btn-primary mt-3 float-end">{t("Let's Talk")}</Button>
             </div>
          </div>
       </section>
